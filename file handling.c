a7d4f31861293b420fe1f5d0969b25d8e78d9eba
timibash <85651144+timibash@users.noreply.github.com>
2022-08-04 20:59:44 +0100

    Add files via upload

---
+++ file handling.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    FILE *myfile;
+    char abdul='B'
+    myfile = fopen("abdul.txt","a");
+    //printf("%d",fputc(abdul,myfile));
+    //abdul = fgetc(myfile);
+    //printf("%c"(abdul);
+    //fputs(abdul,myfile)
+
+    fclose(myfile);
+
+return 0;
+
+}
